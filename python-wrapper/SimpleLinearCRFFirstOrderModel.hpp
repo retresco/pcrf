070d8b2c30e142f60775a74b2a53a33e7e4dc5fc
ThH <Thomas.Hanneforth@uni-potsdam.de>
2015-06-08 11:34:45 +0200

    Delted SimpleLinearCRFRirstOrderModel.hpp

--- python-wrapper/SimpleLinearCRFFirstOrderModel.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __SIMPLE_LINEAR_CRF_FIRST_ORDER_MODEL_HPP__
-#define __SIMPLE_LINEAR_CRF_FIRST_ORDER_MODEL_HPP__
-
-#include <SimpleLinearCRFModel.hpp>
-
-class SimpleLinearCRFFirstOrderModel : public SimpleLinearCRFModel<1> {};
-
-#endif
